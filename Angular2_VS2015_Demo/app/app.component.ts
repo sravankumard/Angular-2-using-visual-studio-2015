fe1f6a165bdf18cb49ee8a684764c6a06d9c4c62
sravan Dudyalu <sravan.kumar.reddy@outlook.com>
2016-10-30 18:20:02 +0530

    first commit

---
+++ Angular2_VS2015_Demo/app/app.component.ts
@@ -0,0 +1,6 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Angular 2 app using visual studio 2015</h1>'
+})
+export class AppComponent { }
---
+++ README.md
@@ -0,0 +1,2 @@
+"# Angular-2-demo-app-using-visual-studio-2015" 
+"Angular2 Demp app using visual stdio 2015" 
